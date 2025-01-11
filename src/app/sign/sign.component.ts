559a5e5846cca1293aa34f1fe334085ad5e784f5
saifmogaadi12 <aboura.joujou@outlook.es>
2025-01-11 21:35:44 +0100

    add RouterLink import and update sign-in form with routerLink for password recovery

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
-  styleUrls: ['./sign.component.css']
+  styleUrls: ['./sign.component.css'],
+  imports: [RouterLink],
 })
-export class SignComponent {
-}
+export class SignComponent {}
