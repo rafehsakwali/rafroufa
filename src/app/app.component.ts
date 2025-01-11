0e6f8e4beb4d9b01278600d559afb80536dc22c9
saifmogaadi12 <aboura.joujou@outlook.es>
2025-01-11 21:47:23 +0100

    add Navbar component and integrate it into the main application layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class AppComponent {
   title = 'new';
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  imports: [RouterLink],
+})
+export class NavbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
